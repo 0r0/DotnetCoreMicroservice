90c1c60255fe0a70b236c819419a607eabfa2c29
mehdi <0r0.developer@gmail.com>
2024-02-02 23:59:49 +0330

    implement product repository

--- Src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ Src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -14,7 +14,7 @@ public class Product
 
     public string  Category { get; set; }
     
-    public string Summery { get; set; }
+    public string Summary { get; set; }
     public string Description { get; set; }
     public string ImageFile { get; set; }
     public decimal Price { get; set; }
---
+++ Src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly ICatalogContext _catalogContext;
+
+    public ProductRepository(ICatalogContext catalogContext)
+    {
+        _catalogContext = catalogContext ?? throw new ArgumentNullException(nameof(catalogContext));
+    }
+
+    public async Task<IEnumerable<Product>> GetProducts()
+    {
+        return await _catalogContext.Products.Find(p => true).ToListAsync();
+    }
+
+    public async Task<Product> GetProductById(string id)
+    {
+        return await _catalogContext.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetProductByName(string productName)
+    {
+        return await _catalogContext.Products.Find(p => p.Name == productName).ToListAsync();
+    }
+
+    public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+    {
+        return await _catalogContext.Products.Find(p => p.Category == categoryName).ToListAsync();
+    }
+
+    public async Task CreateProduct(Product product)
+    {
+        await _catalogContext.Products.InsertOneAsync(product);
+    }
+
+    public async Task<bool> UpdateProduct(Product product)
+    {
+        var updateResult = await _catalogContext.Products.ReplaceOneAsync(a => a.Id == product.Id, product);
+        return updateResult.IsAcknowledged && updateResult.MatchedCount > 0;
+    }
+
+    public async Task<bool> DeleteProduct(string id)
+    {
+        var deleteResult = await _catalogContext.Products.DeleteOneAsync(a => a.Id == id).ConfigureAwait(false);
+        return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+    }
+}
