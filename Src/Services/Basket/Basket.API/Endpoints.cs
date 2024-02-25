cc6f2ac2d85b8ba20e5ab83abe9fab1638107df8
mehdi <0r0.developer@gmail.com>
2024-02-25 23:46:02 +0330

    implement basket route and add repository dependency

--- Src/Services/Basket/Basket.API/Endpoints.cs
+++ Src/Services/Basket/Basket.API/Endpoints.cs
@@ -1,67 +1,41 @@
 ﻿using Asp.Versioning.Builder;
+using Basket.API.Entities;
+using Basket.API.Repositories;
 
 namespace Basket.API;
 
 public static class Endpoints
 {
-    public static void CatalogRoutes(this WebApplication application,
+    public static void BasketRoutes(this WebApplication application,
         ApiVersionSet apiVersionSet)
     {
         RouteGroupBuilder group = application
             .MapGroup("api/v{version:apiVersion}/{basket}")
             .WithApiVersionSet(apiVersionSet);
 
-        group.MapGet("/Basket",
-            async (IBasketRepository repository) => await repository.GetBaskets());
-
-        // group.MapGet("/catalog/{id}",
-        //     async (string id, IBasketRepository repository) => await repository.GetBasketById(id));
-        //
-        // group.MapGet("/catalog/getBasketByCategory/{category}",
-        //     async (string category, IBasketRepository repository) =>
-        //         await repository.GetBasketByCategory(category));
-
-        group.MapPost("/Basket",
-            async (Basket basket, IBasketRepository repository) =>
+        group.MapGet("/Basket/{userName}",
+            async (string userName, IBasketRepository repository) =>
             {
-                await repository.CreateBasket(basket);
-                return Results.Created("id", basket.Id);
+                var basket = await repository.GetBasket(userName);
+                return Results.Ok(basket ?? new ShoppingCart(userName));
+            }
+        );
+
+
+        group.MapPost("/Basket/",
+            async (ShoppingCart shoppingCart, IBasketRepository repository) =>
+            {
+                await repository.UpdateBasket(shoppingCart);
+                return Results.Ok(shoppingCart);
             });
-        group.MapPost("/Basket/Checkkout",
-            async (Basket basket, IBasketRepository repository) =>
+     
+
+
+        group.MapDelete("/basket/{userName}",
+            async (string userName, IBasketRepository repository) =>
             {
-                await repository.CreateBasket(basket);
-                return Results.Created("id", basket.Id);
-            });
-
-        // group.MapPut("/catalog/{id}/{basket}",
-        //     async (string id, Basket basket, IBasketRepository repository) =>
-        //     {
-        //         await repository.UpdateBasket(basket);
-        //         return Results.NoContent();
-        //     });
-
-
-        group.MapDelete("/basket/{id}",
-            async (string id, IBasketRepository repository) =>
-            {
-                await repository.DeleteBasket(id);
-                return Results.NoContent();
+                await repository.Delete(userName);
+                return Results.Ok();
             });
     }
-}
-
-public class Basket
-{
-    public object? Id { get; set; }
-}
-
-public interface IBasketRepository
-{
-    Task<object> GetBaskets();
-    Task GetBasketById(string id);
-    Task CreateBasket(Basket basket);
-    Task UpdateBasket(Basket basket);
-    Task DeleteBasket(string id);
-    Task GetBasketByCategory(string category);
 }
--- Src/Services/Basket/Basket.API/Program.cs
+++ Src/Services/Basket/Basket.API/Program.cs
@@ -1,13 +1,16 @@
+using Basket.API.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddStackExchangeRedisCache(opt=>
-    opt.Configuration =builder.Configuration.GetValue<string>("CacheSetting:ConnectionString")
-    
-    );
+builder.Services.AddStackExchangeRedisCache(opt =>
+    opt.Configuration = builder.Configuration.GetValue<string>("CacheSetting:ConnectionString")
+);
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,6 +23,4 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-
 app.Run();
-
