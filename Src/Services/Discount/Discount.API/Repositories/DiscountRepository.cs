02c59591931eff337a04ed99453d696a470478da
mehdi <0r0.developer@gmail.com>
2024-03-16 01:05:46 +0330

    add DiscountRepository and coupon entity

---
+++ Src/Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,36 @@
+﻿using Discount.API.Entities;
+using Npgsql;
+
+namespace Discount.API.Repositories;
+
+public class DiscountRepository : IDiscountRepository
+{
+    private readonly IConfiguration _configuration;
+
+    public DiscountRepository(IConfiguration configuration)
+    {
+        _configuration = configuration ?? throw new NullReferenceException();
+        using var connection = new NpgsqlConnection(configuration.GetValue<string>("ConnectionStrings"));
+
+    }
+
+    public Task<Coupon> GetDiscount(string productName)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Create(Coupon coupon)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Update(Coupon coupon)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Delete(string productName)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Src/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,11 @@
+﻿using Discount.API.Entities;
+
+namespace Discount.API.Repositories;
+
+public interface IDiscountRepository
+{
+    Task<Coupon> GetDiscount(string productName);
+    Task<bool> Create(Coupon coupon);
+    public Task<bool> Update(Coupon coupon);
+    public Task<bool> Delete(string productName);
+}
