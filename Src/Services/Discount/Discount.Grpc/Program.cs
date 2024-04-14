f5e8959ab6e707b324962b9a4d209931bf60f9c7
mehdi <0r0.developer@gmail.com>
2024-04-15 00:49:55 +0330

    add grpc service dependency

--- Src/Services/Discount/Discount.Grpc/Program.cs
+++ Src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,5 +1,6 @@
 using Discount.Grpc;
 using Discount.Grpc.Repositories;
+using Discount.Grpc.Services;
 
 // using Discount.Grpc.Services;
 
@@ -12,7 +13,7 @@ builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-// app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
