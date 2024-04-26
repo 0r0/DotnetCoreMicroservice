b839c66cd03c36ba00eb2fb652d49d6026aceb28
mehdi <0r0.developer@gmail.com>
2024-04-26 11:15:30 +0330

    update order command handler and validator implementation

---
+++ Src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder;
+
+public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+{
+    private readonly ILogger<UpdateOrderCommandHandler> _logger;
+    private readonly IOrderRepository _orderRepository;
+
+    public UpdateOrderCommandHandler(ILogger<UpdateOrderCommandHandler> logger, IOrderRepository orderRepository)
+    {
+        _logger = logger;
+        _orderRepository = orderRepository;
+    }
+
+    public async Task Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = await _orderRepository.GetByIdAsync(request.Id);
+        if(order is null)
+        {
+
+            // throw new NotFoundException(nameof(Order), request.Id);
+            _logger.LogError("order does not exist in database");
+        }
+        
+        await _orderRepository.UpdateAsync(request.Map());
+        
+        _logger.LogInformation("order with {id} is updated successfully",order.Id);
+
+
+
+    }
+}
