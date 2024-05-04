2ddc6fb4f167425c59a9d48b3318fae6118effb1
mehdi <0r0.developer@gmail.com>
2024-05-05 01:14:10 +0330

    add entity framework and implement db context for order infrastructure

---
+++ Src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entity;
+
+namespace Ordering.Infrastructure.Persistence;
+
+public class OrderContextSeed
+{
+    public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+    {
+        if (!orderContext.Orders.Any())
+        {
+            await orderContext.Orders.AddRangeAsync(PreConfiguredOrder());
+            await orderContext.SaveChangesAsync();
+            logger.LogInformation("seed database associated by {dbContext}",nameof(OrderContext));
+        }
+    }
+
+    private static Order[] PreConfiguredOrder()
+    {
+        return new[]
+        {
+            new Order()
+            {
+                Country = "Iran",
+                Expiration = "Unknown",
+                State = "Tehran",
+                AddressLine = "Tehran.AmirAbad",
+                CardName = "Mehdi",
+                CardNumber = "1234",
+                FirstName = "Mehdi",
+                EmailAddress = "Mehdi.goharinezhad@gmail.com",
+                LastName = "Goharinezhad",
+                ZipCode = "1234",
+                PaymentMethod = 1,
+                TotalPrice = 1000000,
+                UserName = "swn",
+                CVV = "313"
+            }
+        };
+    }
+}
---
+++ Src/Services/Ordering/Ordering.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq.Expressions;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Common;
+using Ordering.Infrastructure.Persistence;
+
+namespace Ordering.Infrastructure.Repositories;
+
+public class RepositoryBase<T> :IAsyncRepository<T> where T: EntityBase
+{
+    private readonly OrderContext _dbContext;
+
+    public RepositoryBase(OrderContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeString = null,
+        bool disableTracking = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<Expression<Func<T, object>>> includes = null, bool disableTracking = true)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> AddAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+}
