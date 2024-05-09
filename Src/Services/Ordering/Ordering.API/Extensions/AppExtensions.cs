79420b9bb58b273a61de53dffdb9ed8a1d397cd2
mehdi <0r0.developer@gmail.com>
2024-05-09 12:40:33 +0330

    define MigrateDatabase for db context

---
+++ Src/Services/Ordering/Ordering.API/Extensions/AppExtensions.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ordering.API.Extensions;
+
+public static class AppExtensions
+{
+    public static void MigrateDatabase<TContext>(this WebApplication application,
+        Action<TContext,IServiceProvider> seeder,int? retry = 0) where TContext:DbContext
+    {
+        int retryForAvailability = retry.Value;
+        using (var scope =application.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var logger = services.GetRequiredService<ILogger<TContext>>();
+            var context = services.GetService<TContext>();
+            try
+            {
+                logger.LogInformation("Migration database with association context {DbContextName}",nameof(TContext));
+                InvokeSeeder(seeder,context,services);
+            }
+            catch (SqlException e)
+            {
+                logger.LogError(e,"an error occured while migrating the database using in context");
+                if (retryForAvailability < 50)
+                {
+                    retryForAvailability++;
+                    Thread.Sleep(2000);
+                    MigrateDatabase(application,seeder,retryForAvailability);
+                }
+            }
+        }
+        
+    }
+
+    private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services) where TContext : DbContext
+    {
+        context.Database.Migrate();
+        seeder(context, services);
+    }
+}
