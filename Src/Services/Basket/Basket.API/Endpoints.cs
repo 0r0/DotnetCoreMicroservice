5174e3e9175f937af36093c3ccd601435e90934a
mehdi <0r0.developer@gmail.com>
2024-05-11 23:26:32 +0330

    create BasketCheckout and publish it on rabbitmq

--- Src/Services/Basket/Basket.API/Endpoints.cs
+++ Src/Services/Basket/Basket.API/Endpoints.cs
@@ -2,6 +2,8 @@
 using Basket.API.Entities;
 using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using EventBus.Messages.Events;
+using MassTransit;
 
 namespace Basket.API;
 
@@ -24,7 +26,7 @@ public static class Endpoints
 
 
         group.MapPost("/Basket/",
-            async (ShoppingCart shoppingCart, IBasketRepository repository,DiscountGrpcService service) =>
+            async (ShoppingCart shoppingCart, IBasketRepository repository, DiscountGrpcService service) =>
             {
                 //get discount coupon amount and subtract from shopping cards item price
                 foreach (var item in shoppingCart.Items)
@@ -32,10 +34,10 @@ public static class Endpoints
                     var coupon = await service.GetDiscount(item.ProductName);
                     item.Price -= coupon.Amount;
                 }
+
                 await repository.UpdateBasket(shoppingCart);
                 return Results.Ok(shoppingCart);
             });
-     
 
 
         group.MapDelete("/basket/{userName}",
@@ -44,5 +46,33 @@ public static class Endpoints
                 await repository.Delete(userName);
                 return Results.Ok();
             });
+        group.MapPost("/checkout/{checkOut}", async (BasketCheckout basketCheckout, IBasketRepository repository,IPublishEndpoint publish) =>
+        {
+            var basket = await repository.GetBasket(basketCheckout.UserName);
+            if (basket == null)
+                return Results.BadRequest();
+            var basketEvent = new BasketCheckoutEvent()
+            {
+                Country = basketCheckout.Country,
+                Expiration = basketCheckout.Expiration,
+                State = basketCheckout.State,
+                AddressLine = basketCheckout.AddressLine,
+                CardName = basketCheckout.CardName,
+                UserName = basketCheckout.UserName,
+                CardNumber = basketCheckout.CardName,
+                EmailAddress = basketCheckout.EmailAddress,
+                FirstName = basketCheckout.FirstName,
+                LastName = basketCheckout.LastName,
+                PaymentMethod = basketCheckout.PaymentMethod,
+                TotalPrice = basket.TotalPrice,
+                ZipCode = basketCheckout.ZipCode,
+                CVV = basketCheckout.CVV
+            };
+            // dispatch event 
+            await publish.Publish(basketEvent);
+            await repository.Delete(basketCheckout.UserName);
+            
+            return Results.Accepted();
+        }).WithName("basketCheckout");
     }
 }
