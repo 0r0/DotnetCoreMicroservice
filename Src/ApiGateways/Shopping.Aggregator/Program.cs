304b0b7068cfc81b9bb672f4f5b89c900cbbf66d
mehdi <0r0.developer@gmail.com>
2024-05-17 17:24:57 +0330

    initiate services and add them as httpclient dependency

--- Src/ApiGateways/Shopping.Aggregator/Program.cs
+++ Src/ApiGateways/Shopping.Aggregator/Program.cs
@@ -1,10 +1,20 @@
+using Shopping.Aggregator.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddHttpClient<ICatalogService, CatalogService>(c => c.BaseAddress =
+    new Uri(builder.Configuration.GetValue<string>("ApiSettings:CatalogUrl") ??
+            throw new ArgumentNullException(nameof(c), "api settings for order can not be null")));
+builder.Services.AddHttpClient<IBasketService, BasketService>(c => c.BaseAddress =
+    new Uri(builder.Configuration.GetValue<string>("ApiSettings:BasketUrl") ??
+            throw new ArgumentNullException(nameof(c), "api settings for order can not be null")));
+builder.Services.AddHttpClient<IOrderService, OrderService>(c => c.BaseAddress =
+    new Uri(builder.Configuration.GetValue<string>("ApiSettings:OrderingUrl") ??
+            throw new ArgumentNullException(nameof(c), "api settings for order can not be null")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -17,8 +27,4 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 
-
-
-
 app.Run();
-
