c4591be010398d6e65e8700e3903ef570e59ad29
mehdi <0r0.developer@gmail.com>
2024-05-17 16:31:47 +0330

    add shopping aggregator project and define related models

---
+++ Src/ApiGateways/Shopping.Aggregator/Models/BasketModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Shopping.Aggregator.Models;
+
+public class BasketModel
+{
+    public string UserName { get; set; }
+    public IReadOnlyCollection<BasketItemExtendedModel> Items { get; set; } = new List<BasketItemExtendedModel>();
+
+
+    public decimal TotalPrice { get; set; }
+}
