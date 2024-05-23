094cd73320991fafd636772c4a29dab0abda0b6e
mehdi <0r0.developer@gmail.com>
2024-05-23 16:36:34 +0330

    add webapps project

---
+++ Src/WebApps/AspNetRunBasic/Seeding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AspNetRunBasic.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetRunBasic;
+
+public static class Seeding
+{
+    public static void SeedDatabase(this IHost host)
+    {
+        using (var scope = host.Services.CreateScope())
+        {
+            var services = scope.ServiceProvider;
+            var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+
+            try
+            {
+                var aspnetRunContext = services.GetRequiredService<AspnetRunContext>();
+                AspnetRunContextSeed.SeedAsync(aspnetRunContext, loggerFactory).Wait();
+            }
+            catch (Exception exception)
+            {
+                var logger = loggerFactory.CreateLogger<Program>();
+                logger.LogError(exception, "An error occurred seeding the DB.");
+            }
+        }
+    }
+}
