d41f34148a6e07fecbde31328039fc4516c37c61
mehdi <0r0.developer@gmail.com>
2024-07-18 11:29:24 +0330

    add serilogger class a

---
+++ Src/BuildingBlocks/Common.Logging/SeriLogger.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using Elastic.Ingest.Elasticsearch;
+using Elastic.Ingest.Elasticsearch.DataStreams;
+using Elastic.Serilog.Sinks;
+using Elastic.Transport;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Common.Logging;
+
+public static class SeriLogger
+{
+    public static Action<HostBuilderContext, LoggerConfiguration> Configure => ((context, configuration) =>
+    {
+        configuration.Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch(new[] {new Uri(context.Configuration["ElasticConfiguration:Uri"])}, opt =>
+                {
+                    opt.DataStream = new 
+                        DataStreamName("logs", "appLogs", 
+                            Assembly.GetExecutingAssembly()
+                                .GetName().Name.ToLower()
+                                .Replace(".", "-") + "-" +
+                            context.HostingEnvironment.EnvironmentName
+                                .ToLower().Replace(".", "-") + "-logs-" +
+                            "" + DateTime.UtcNow.ToString("yyyy-MM"));
+                    opt.BootstrapMethod = BootstrapMethod.Failure;
+                },
+                transport =>
+                {
+                    transport.Authentication(new BasicAuthentication(context.Configuration["ElasticConfiguration:UserName"],
+                        context.Configuration["ElasticConfiguration:Password"]));
+                }).Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
+            .ReadFrom.Configuration(context.Configuration);
+    });
+}
