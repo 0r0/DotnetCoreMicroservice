8433ba511452ddb279e0b1d0e43c94fa569eeedb
mehdi <0r0.developer@gmail.com>
2024-08-08 23:00:10 +0330

    add retry poly policy for MigrateDatabase of Discount.API project and fix elastic search settings and add healthcheck fo mongodb in Catalog.API project

--- Src/Services/Catalog/Catalog.API/Program.cs
+++ Src/Services/Catalog/Catalog.API/Program.cs
@@ -3,11 +3,17 @@ using Asp.Versioning.Builder;
 using Catalog.API.Data;
 using Catalog.API.Repositories;
 using Common.Logging;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Serilog;
 using static Catalog.API.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<LoggingDelegatingHandler>();
 builder.Host.UseSerilog(SeriLogger.Configure);
+builder.Services.AddHealthChecks().AddMongoDb(
+    builder.Configuration.GetValue<string>("DatabaseSettings:ConnectionStrings")
+    ,"catalog mongodb health",HealthStatus.Degraded);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddApiVersioning(options =>
 {
@@ -26,7 +32,11 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ICatalogContext, CatalogContext>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
-
+app.MapHealthChecks("/hc",new HealthCheckOptions()
+{
+    Predicate = _=>true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
--- Src/Services/Discount/Discount.API/Extensions/Migration.cs
+++ Src/Services/Discount/Discount.API/Extensions/Migration.cs
@@ -1,53 +1,63 @@
 ﻿using Npgsql;
+using Polly;
 
 namespace Discount.API;
 
 public static class Migration
 {
-    public static void MigrateDatabase(this WebApplication application, int? retry = 0)
+    public static void MigrateDatabase(this WebApplication application)
     {
         using (var scope = application.Services.CreateScope())
         {
-            var retryAvailability = retry.Value;
             var services = scope.ServiceProvider;
             var configuration = services.GetRequiredService<IConfiguration>();
             var logger = services.GetRequiredService<ILogger<Program>>();
             try
             {
                 logger.LogInformation("Migrating postgresql database");
-                using var connection =
-                    new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-                connection.Open();
-                using var command = new NpgsqlCommand()
-                {
-                    Connection = connection
-                };
+                var retry = Policy.Handle<NpgsqlException>()
+                    .WaitAndRetry(
+                        retryCount: 5,
+                        sleepDurationProvider: (retryAttempt) => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                        onRetry: (exception, retryCount, context) =>
+                        {
+                            logger.LogError(
+                                $"Retry {retryCount} of {context.PolicyKey} at {context.OperationKey}, due to: {exception}.");
+                        });
 
-                command.CommandText = "DROP TABLE  IF EXISTS COUPON";
-                command.ExecuteNonQuery();
-                command.CommandText =
-                    "CREATE TABLE COUPON(Id SERIAL PRIMARY KEY ,ProductName VARCHAR(24) NOT NULL ,Description TEXT," +
-                    "Amount INT)";
-                command.ExecuteNonQuery();
-
-                command.CommandText =
-                    "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
-                command.ExecuteNonQuery();
-
-                command.CommandText =
-                    "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
-                command.ExecuteNonQuery();
+                retry.Execute(() => ExecuteMigrations(configuration));
             }
             catch (NpgsqlException exception)
             {
                 Console.WriteLine(exception);
                 logger.LogError(exception, "an error occurred while migrating postgresql database");
-                if (retryAvailability < 50)
-                {
-                    Thread.Sleep(2000);
-                    MigrateDatabase(application, ++retryAvailability);
-                }
             }
         }
+    }
+
+    private static void ExecuteMigrations(IConfiguration configuration)
+    {
+        using var connection =
+            new NpgsqlConnection(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        connection.Open();
+        using var command = new NpgsqlCommand()
+        {
+            Connection = connection
+        };
+
+        command.CommandText = "DROP TABLE  IF EXISTS COUPON";
+        command.ExecuteNonQuery();
+        command.CommandText =
+            "CREATE TABLE COUPON(Id SERIAL PRIMARY KEY ,ProductName VARCHAR(24) NOT NULL ,Description TEXT," +
+            "Amount INT)";
+        command.ExecuteNonQuery();
+
+        command.CommandText =
+            "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('IPhone X', 'IPhone Discount', 150);";
+        command.ExecuteNonQuery();
+
+        command.CommandText =
+            "INSERT INTO Coupon(ProductName, Description, Amount) VALUES('Samsung 10', 'Samsung Discount', 100);";
+        command.ExecuteNonQuery();
     }
 }
