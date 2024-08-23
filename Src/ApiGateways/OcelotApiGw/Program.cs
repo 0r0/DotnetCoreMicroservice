57a4018fdd33d8c534524e86d77854ba342e9dd7
mehdi <0r0.developer@gmail.com>
2024-08-23 12:43:21 +0330

    add json settings config

--- Src/ApiGateways/OcelotApiGw/Program.cs
+++ Src/ApiGateways/OcelotApiGw/Program.cs
@@ -5,17 +5,11 @@ using Ocelot.Middleware;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-// builder.Host.ConfigureLogging(((context, loggingBuilder) =>
-//         {
-//             loggingBuilder.AddConfiguration(context.Configuration.GetSection("Logging"));
-//             loggingBuilder.AddConsole();
-//             loggingBuilder.AddDebug();
-//         }
-//         ));
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
 builder.Services.AddTransient<LoggingDelegatingHandler>();
 builder.Host.UseSerilog(SeriLogger.Configure);
 builder.Services.AddOcelot().AddCacheManager(settings
-    =>settings.WithDictionaryHandle());
+    => settings.WithDictionaryHandle());
 var app = builder.Build();
 
 
